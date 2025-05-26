667a7f9c4aa07c2d0f6bb8ceb2733cf0e6a93576
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-05-26 13:58:16 -0700

    Set stack suffix to empty string if on main branch

--- app.py
+++ app.py
@@ -21,11 +21,12 @@ aws_env = cdk.Environment(
 stack_suffix = app.node.try_get_context("stack-suffix") or ""
 stack_name_prefix = "ArcaneScribeStack"
 
-if stack_suffix:
+if stack_suffix != "main":
     final_stack_name = f"{stack_name_prefix}-{stack_suffix}"
 else:
     # Fallback for local development or main branch if no suffix is provided
     # Consider a more explicit way to differentiate main vs. feature if needed
+    stack_suffix = ""
     final_stack_name = f"{stack_name_prefix}-Main"
 
 

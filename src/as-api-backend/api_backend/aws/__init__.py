746b9952aedb5c0d7a0242868d0298d1078433bf
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-06-14 10:38:26 -0700

    Update aws init to import new ssm client class

--- src/as-api-backend/api_backend/aws/__init__.py
+++ src/as-api-backend/api_backend/aws/__init__.py
@@ -1,18 +1,20 @@
 """AWS module for the Arcane Scribe API Backend.
 
 This module provides AWS-related functionality for the Arcane Scribe API
-Backend, including clients for DynamoDB, S3, and Bedrock. It allows for easy
-access to these services without needing to import them individually in other
-parts of the codebase.
+Backend, including clients for DynamoDB, SSM, S3, and Bedrock. It allows for
+easy access to these services without needing to import them individually in
+other parts of the codebase.
 """
 
-# Local Modules
+# Third Party
 from api_backend.aws.s3 import S3Client
+from api_backend.aws.ssm import SsmClient
 from api_backend.aws.dynamodb import DynamoDb
 from api_backend.aws.bedrock_runtime import BedrockRuntimeClient
 
 __all__ = [
-    "DynamoDb",
     "S3Client",
+    "SsmClient",
+    "DynamoDb",
     "BedrockRuntimeClient",
 ]

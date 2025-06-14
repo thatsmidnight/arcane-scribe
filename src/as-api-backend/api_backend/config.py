6a5b5d9bf7947928b37bc33625094a4eb5aec0a1
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-06-14 10:22:54 -0700

    Change from get since we're defining the lambda and we know what env vars it will have

--- src/as-api-backend/api_backend/config.py
+++ src/as-api-backend/api_backend/config.py
@@ -8,13 +8,11 @@ including the S3 bucket name, DynamoDB table name, and Bedrock model ID.
 import os
 
 # Environment variables for configuration
-API_PREFIX = os.environ.get("API_PREFIX", "/api/v1")
-DOCUMENTS_BUCKET = os.environ.get("DOCUMENTS_BUCKET_NAME")
-VECTOR_STORE_BUCKET_NAME = os.environ.get("VECTOR_STORE_BUCKET_NAME")
-QUERY_CACHE_TABLE_NAME = os.environ.get("QUERY_CACHE_TABLE_NAME")
-BEDROCK_EMBEDDING_MODEL_ID = os.environ.get(
-    "BEDROCK_EMBEDDING_MODEL_ID", "amazon.titan-text-express-v1"
-)
-BEDROCK_TEXT_GENERATION_MODEL_ID = os.environ.get(
-    "BEDROCK_TEXT_GENERATION_MODEL_ID", "amazon.titan-text-express-v1"
-)
+API_PREFIX = os.environ["API_PREFIX"]
+DOCUMENTS_BUCKET = os.environ["DOCUMENTS_BUCKET_NAME"]
+VECTOR_STORE_BUCKET_NAME = os.environ["VECTOR_STORE_BUCKET_NAME"]
+QUERY_CACHE_TABLE_NAME = os.environ["QUERY_CACHE_TABLE_NAME"]
+BEDROCK_EMBEDDING_MODEL_ID = os.environ["BEDROCK_EMBEDDING_MODEL_ID"]
+BEDROCK_TEXT_GENERATION_MODEL_ID = os.environ[
+    "BEDROCK_TEXT_GENERATION_MODEL_ID"
+]

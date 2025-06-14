78642101958e3439644bf5df5f76347c109cdbe1
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-06-14 11:49:16 -0700

    Add new response source enum to init

--- src/as-api-backend/api_backend/utils/__init__.py
+++ src/as-api-backend/api_backend/utils/__init__.py
@@ -4,7 +4,7 @@ This module provides helper functions that can be used across the API backend.
 """
 
 # Local Modules
-from api_backend.utils.enums import AllowedMethod
+from api_backend.utils.enums import AllowedMethod, ResponseSource
 from api_backend.utils.rag_query_processor import get_answer_from_rag
 from api_backend.utils.presigned_url_generator import generate_presigned_url
 
@@ -12,4 +12,5 @@ __all__ = [
     "get_answer_from_rag",
     "generate_presigned_url",
     "AllowedMethod",
+    "ResponseSource",
 ]

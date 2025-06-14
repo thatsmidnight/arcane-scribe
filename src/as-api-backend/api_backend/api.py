cb41ee4a3820f79b5a0abfebbb2992aa1d4360fc
thatsmidnight <10793854+thatsmidnight@users.noreply.github.com>
2025-06-14 10:19:31 -0700

    Apply ip verification to router instead of repeating it on each other router

--- src/as-api-backend/api_backend/api.py
+++ src/as-api-backend/api_backend/api.py
@@ -1,11 +1,12 @@
 # Third Party
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 # Local Modules
 from api_backend.routers import query, srd
+from api_backend.dependencies import verify_source_ip
 
 # Create a router instance with a default prefix
-router = APIRouter()
+router = APIRouter(dependencies=[Depends(verify_source_ip)])
 
 # Include other routers
 router.include_router(query.router)
